c2753a36ba1ddf3f78acdcad8d8820acacd1c0e2
TheSuspect <TheSuspect9980@gmail.com>
2024-11-04 00:10:20 +0300

    Initial commit

---
+++ nou_client/GameEngine.cs
@@ -0,0 +1,62 @@
+using System;
+
+namespace NoUC;
+
+public class GameEngine
+{
+    private static readonly CancellationTokenSource cts = new CancellationTokenSource();
+    private bool isRunning = true;
+    private int frameRate = 100;
+    
+    public GameEngine()
+    {
+        Console.CancelKeyPress += (sender, args) => Exit();
+    }
+
+    public void Run()
+    {
+        while (isRunning)
+        {
+            if(Console.KeyAvailable) Input();
+
+
+            Thread.Sleep(frameRate);
+        }
+    }
+
+    private void Input()
+    {
+        ConsoleKeyInfo key = Console.ReadKey(true);
+
+        switch (key.Key)
+        {
+            case ConsoleKey.Q:
+                isRunning = false;
+                Exit();
+                return;
+
+            case ConsoleKey.DownArrow:
+                break;
+
+            case ConsoleKey.Spacebar:
+                break;
+                
+            default:
+                break;
+        }
+    }
+    
+    private void Exit()
+    {
+
+        //Log("Exit!");
+
+        Console.WriteLine("Exiting...");
+        cts.Cancel();
+        Console.Clear();
+    }
+    private void Log(string msg)
+    {
+        File.AppendAllText("log.txt", System.Environment.NewLine + System.DateTime.Now + " # " + msg);
+    }
+}
---
+++ nou_client/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace NoUC;
+
+public class Program
+{
+    public static void Main(string[] args)
+    {
+        GameEngine game = new GameEngine();
+        game.Run();
+    }
+}
